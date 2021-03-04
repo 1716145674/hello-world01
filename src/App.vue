8ce269ea0a9548fc7a95a88216b26e2691a48622
zqq <1716145674@qq.com>
2021-03-04 15:54:50 +0800

    'fuck'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-      <p>java0821 全体同学 前程似锦</p>
-      <p>dev分支建立了</p>
-      <p>测试dev分支第一次提交</p>
-   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+      <h1 style="align-content: center">Test</h1>
+      <!--   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>-->
+      <img src="/data/static/QQ图片20210304143043.jpg">
   </div>
 </template>
 
